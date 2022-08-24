4f20e69da1ad2d63a0bb66021a48790b8951f0f1
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-08-23 21:05:24 -0600

    Add files via upload

---
+++ PlayListMundial/Aire Arcano/js/music-list.js
@@ -0,0 +1,31 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "No Quiero Más",
+        artist: "Aire Arcano",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Si te Viera Smith",
+        artist: "Aire Arcano",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "La Mona Lisa",
+        artist: "Aire Arcano",
+        img: "music-3",
+        src: "music-3"
+    },
+
+];
