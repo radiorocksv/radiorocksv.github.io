db8bc68e37d20a8c542e413aeeb18ca9702fded8
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-08-23 20:13:02 -0600

    Add files via upload

---
+++ PlayListMundial/1970/js/music-list.js
@@ -0,0 +1,49 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Altar del Diablo",
+        artist: "1970",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "El Ángel de la Oscuridad",
+        artist: "1970",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "El Señor de las Moscas",
+        artist: "1970",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hombres Perro",
+        artist: "1970",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jesus Christ",
+        artist: "1970",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Vlad el Empalador",
+        artist: "1970",
+        img: "music-6",
+        src: "music-6"
+    },
+
+];
