d627a036fd5026f996b123f3edc46a70906f5b54
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-09-07 15:43:06 -0600

    Add files via upload

---
+++ PlayListMundial/AnarkiaHN/js/music-list.js
@@ -0,0 +1,91 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Infinito",
+        artist: "Anarkia",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Dejame LLegar",
+        artist: "Anarkia",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Destino",
+        artist: "Anarkia",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Enigma",
+        artist: "Anarkia",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Esclavitud",
+        artist: "Anarkia",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Fría Ilusión",
+        artist: "Anarkia",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Fuego",
+        artist: "Anarkia",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Guerrero",
+        artist: "Anarkia",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Hacia La Eternidad",
+        artist: "Anarkia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Heavy Metal",
+        artist: "Anarkia",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Prisionero",
+        artist: "Anarkia",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Sed I",
+        artist: "Anarkia",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Sed II",
+        artist: "Anarkia",
+        img: "music-13",
+        src: "music-13"
+    },
+
+];
---
+++ PlayListMundial/AuroraVoraz/js/music-list.js
@@ -0,0 +1,31 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Mejor Sola",
+        artist: "Aurora Voraz",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Al Acecho",
+        artist: "Aurora Voraz",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Circulo Fecundo",
+        artist: "Aurora Voraz",
+        img: "music-3",
+        src: "music-3"
+    },
+
+];
