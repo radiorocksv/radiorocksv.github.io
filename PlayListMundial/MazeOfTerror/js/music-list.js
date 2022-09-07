a95bf830e7bb544c89ceafb4dbc95a16f1591ff0
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-09-07 16:51:36 -0600

    Add files via upload

---
+++ PlayListMundial/MazeOfTerror/js/music-list.js
@@ -0,0 +1,43 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "From Beyond",
+        artist: "Maze Of Terror",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Hatred and Repression",
+        artist: "Maze Of Terror",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Incubus",
+        artist: "Maze Of Terror",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Lycanthropes",
+        artist: "Maze Of Terror",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "The Hunger",
+        artist: "Maze Of Terror",
+        img: "music-5",
+        src: "music-5"
+    },
+    
+];
