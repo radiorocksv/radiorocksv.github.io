a0fbdeda8f049d3546c383e079d7ef5f2607db5c
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-09-07 16:02:11 -0600

    Add files via upload

---
+++ PlayListMundial/DeepSilent/js/music-list.js
@@ -0,0 +1,49 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "In The Eden Fields",
+        artist: "Deep Silent",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "The Plague",
+        artist: "Deep Silent",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Domi-Nación",
+        artist: "Deep Silent",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "In The Name Of...",
+        artist: "Deep Silent",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "G.O.D. (Grotesque Obsessive Dictator)",
+        artist: "Deep Silent",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Morbidos Palimpsestos",
+        artist: "Deep Silent",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+];
