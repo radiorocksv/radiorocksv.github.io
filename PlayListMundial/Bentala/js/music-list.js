28513ee2ec008808754be0e86e528c8d75805ac5
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2022-09-07 15:54:33 -0600

    Add files via upload

---
+++ PlayListMundial/Bentala/js/music-list.js
@@ -0,0 +1,25 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Gegar Otak",
+        artist: "Bentala",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Paham Karma",
+        artist: "Bentala",
+        img: "music-2",
+        src: "music-2"
+    },
+  
+    ];
---
+++ PlayListMundial/Cernunnos/js/music-list.js
@@ -0,0 +1,67 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "The Crowning",
+        artist: "Cernunnos",
+        img: "music-1",
+        src: "music-1"
+    },
+{
+        name: "Torc",
+        artist: "Cernunnos",
+        img: "music-2",
+        src: "music-2"
+    },
+{
+        name: "Capricornvs",
+        artist: "Cernunnos",
+        img: "music-3",
+        src: "music-3"
+    },
+{
+        name: "The Svmmoner",
+        artist: "Cernunnos",
+        img: "music-4",
+        src: "music-4"
+    },
+{
+        name: "Durno",
+        artist: "Cernunnos",
+        img: "music-5",
+        src: "music-5"
+    },
+{
+        name: "CuChulainn",
+        artist: "Cernunnos",
+        img: "music-6",
+        src: "music-6"
+    },
+{
+        name: "Ain´t No Hero",
+        artist: "Cernunnos",
+        img: "music-7",
+        src: "music-7"
+    },
+{
+        name: "The Arcane Below",
+        artist: "Cernunnos",
+        img: "music-8",
+        src: "music-8"
+    },
+{
+        name: "Survivors",
+        artist: "Cernunnos",
+        img: "music-9",
+        src: "music-9"
+    },
+    
+    ];
