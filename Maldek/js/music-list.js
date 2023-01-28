b331c85ad2e4edd313df0c10aee0378502f7434a
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2023-01-27 19:04:46 -0600

    Add files via upload

---
+++ Maldek/js/music-list.js
@@ -0,0 +1,36 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Caos en Mi Mente",
+        artist: "Deshumanizados",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Bastardos",
+        artist: "Pesadilla Sangrienta",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Filo de la Maldad",
+        artist: "Pesadilla Sangrienta",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Cristo Engusadano",
+        artist: "Pesadilla Sangrienta",
+        img: "music-4",
+        src: "music-4"
+    },
+];
