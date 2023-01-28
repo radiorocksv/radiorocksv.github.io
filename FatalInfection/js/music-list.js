ab57627ff878a376b97cd5341fdb0a2e6c5d833c
Radio Rock SV <100548058+radiorocksv@users.noreply.github.com>
2023-01-27 19:35:05 -0600

    Add files via upload

---
+++ FatalInfection/js/music-list.js
@@ -0,0 +1,37 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Tierra de Miserias - Balas de Cañon",
+        artist: "Fatal Infection",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Tierra de Miserias - Súpervivientes",
+        artist: "Fatal Infection",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tierra de Miserias - Sangre en el asfalto",
+        artist: "Fatal Infeccion",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tierra de Miserias - Infección Fatal",
+        artist: "Fatal Infection",
+        img: "music-4",
+        src: "music-4"
+    },
+
+];
